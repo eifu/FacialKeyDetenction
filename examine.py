dc3a91fa425d122fbada24a6a1c83e98458404c6
eifu <etomita18@gmail.com>
2016-10-09 23:20:03 -0400

    reduce data. convert XY data to one, col=30 to col=17, remove missing data, from row=7000 to row=2000

--- examine.py
+++ examine.py
@@ -11,6 +11,12 @@ df = df.dropna()
 image_series = df["Image"].apply(lambda im: np.fromstring(im, dtype=np.int64, sep=" "))
 
 
+def convertXYColumns(x_list, y_list):
+    result = np.zeros(2140)
+    for i, x_elem in enumerate(x_list):
+            result[i] = x_elem + y_list[i]*96 # the image is 96 by 96
+    return result
+
 
 left_eye_center = convertXYColumns(df["left_eye_center_x"].tolist(),df["left_eye_center_y"].tolist())
 right_eye_center = convertXYColumns(df["right_eye_center_x"].tolist(),df["right_eye_center_y"].tolist())
@@ -35,7 +41,7 @@ data_array = np.array([left_eye_center, right_eye_center,
                        left_eyebrow_inner_end, left_eyebrow_outer_end, 
                        right_eyebrow_inner_end, right_eyebrow_outer_end, 
                        nose_tip, mouth_left_corner, mouth_right_corner, 
-                       mouth_center_top_lip, mouth_center_bottom_lip, image_series])
+                       mouth_center_top_lip, mouth_center_bottom_lip])
 data_array = data_array.T
 
 
@@ -44,23 +50,28 @@ from sklearn.cluster import KMeans
 
 cluster_value = KMeans(n_clusters=4).fit_predict(data_array)
 
-clusterinfo = pd.DataFrame()
-clusterinfo["left_eye_center"]=left_eye_center
-clusterinfo["right_eye_center"]= right_eye_center
-clusterinfo["left_eye_inner_corner"]=left_eye_inner_corner
-clusterinfo["left_eye_outer_corner"]=left_eye_outer_corner 
-clusterinfo["right_eye_inner_corner"]=right_eye_inner_corner
-clusterinfo["right_eye_outer_corner"]=right_eye_outer_corner
-clusterinfo["left_eyebrow_inner_end"]=left_eyebrow_inner_end
-clusterinfo["left_eyebrow_outer_end"]=left_eyebrow_outer_end 
-clusterinfo["right_eyebrow_inner_end"]=right_eyebrow_inner_end
-clusterinfo["right_eyebrow_outer_end"]=right_eyebrow_outer_end
-clusterinfo["nose_tip"]=nose_tip
-clusterinfo["mouth_left_corner"]=mouth_left_corner
-clusterinfo["mouth_right_corner"]=mouth_right_corner
-clusterinfo["mouth_center_top_lip"]=mouth_center_top_lip
-clusterinfo["mouth_center_bottom_lip"]=mouth_center_bottom_lip
-clusterinfo["Image"] = image_series
-clusterinfo["cluster_id"]=cluster_value
+save_array = np.array([left_eye_center, right_eye_center, 
+                       left_eye_inner_corner, left_eye_outer_corner, 
+                       right_eye_inner_corner, right_eye_outer_corner, 
+                       left_eyebrow_inner_end, left_eyebrow_outer_end, 
+                       right_eyebrow_inner_end, right_eyebrow_outer_end, 
+                       nose_tip, mouth_left_corner, mouth_right_corner, 
+                       mouth_center_top_lip, mouth_center_bottom_lip, 
+                       image_series, cluster_value])
 
-clusterinfo.to_csv("test.csv", header=header)
+
+data_array = data_array.T
+import csv
+header = ["left_eye_center", "right_eye_center", 
+          "left_eye_inner_corner", "left_eye_outer_corner", 
+          "right_eye_inner_corner", "right_eye_outer_corner", 
+          "left_eyebrow_inner_end", "left_eyebrow_outer_end", 
+          "right_eyebrow_inner_end", "right_eyebrow_outer_end", 
+          "nose_tip", "mouth_left_corner", "mouth_right_corner", 
+          "mouth_center_top_lip", "mouth_center_bottom_lip", "Image", "cluster_id"]
+f = open('test.csv', 'w')
+
+writer = csv.writer(f, lineterminator='\n')
+writer.writerow(header)
+writer.writerows(data_array)
+f.close()
